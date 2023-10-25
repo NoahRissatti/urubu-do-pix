de7517e3fb2e6bb8c855b4d958deb4106b8a9905
unknown <noah.rissatii@gmail.com>
2023-10-25 20:25:43 -0300

    feat: adicionando axios nas dependencias

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -16,6 +16,18 @@ import { TitledInput } from "../../components/TitledInput";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
 
+import axios from 'axios';
+
+const apiUrl = 'http://localhost:3001';
+
+axios.get(`${apiUrl}/posts`)
+  .then(response => {
+    // Faça algo com os dados
+  })
+  .catch(error => {
+    // Trate os erros
+  });
+
 export const Login: React.FC = () => {
   return (
     <Container>
