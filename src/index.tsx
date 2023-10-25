2267b66caa9c55641933afd08355134aa757c8c9
unknown <noah.rissatii@gmail.com>
2023-10-25 19:58:32 -0300

    feat: adicionando rotas pra paginas login e cadastro

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Router, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Login } from "./pages/Login";
+import { Register } from "./pages/Register";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login/>,
+  },
+  {
+    path: "register",
+    element: <Register />,
+  },
+]);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+    <React.StrictMode>
+     <RouterProvider router={router} />
+    </React.StrictMode>
+ 
 );
 
 // If you want to start measuring performance in your app, pass a function
