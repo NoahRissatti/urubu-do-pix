434318a37d1a2ec3d2910eb994ca2f759cbfc173
unknown <noah.rissatii@gmail.com>
2023-10-25 18:38:42 -0300

    feat: criando projeto base

---
+++ src/components/TitledInput/index.tsx
@@ -0,0 +1,11 @@
+// External Libraries
+import React from 'react'
+
+// Components
+
+// Styles
+import { Container } from './styles'
+
+export const TitledInput: React.FC = () => {
+  return <Container>{/* Code */}</Container>
+}
