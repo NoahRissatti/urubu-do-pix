487ead6808cea3dfef1471a2c762105453408cd6
unknown <noah.rissatii@gmail.com>
2023-10-25 19:30:47 -0300

    feat: criando tela de cadastro

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,22 @@
 // External Libraries
-import React from 'react'
+import React from "react";
 
 // Components
 
 // Styles
-import { Container } from './styles'
+import { Container, Content, Title } from "./styles";
+import { TitledInput } from "../../components/TitledInput";
 
 export const Register: React.FC = () => {
-  return <Container>{/* Code */}</Container>
-}
+  return (
+    <Container>
+      <Content>
+        <Title>Cadastre-se para utilizar o Urubo do Pix</Title>
+        <TitledInput label={"Nome"} placeholder="Insira seu nome" />
+        <TitledInput label={"Email"} placeholder="Insira seu email" />
+        <TitledInput label={"Senha"} placeholder="Insira sua senha" />
+        <TitledInput label={"Chave do Pix"} placeholder="Insira sua chave do pix" />
+      </Content>
+    </Container>
+  );
+};
--- src/pages/Register/styles.ts
+++ src/pages/Register/styles.ts
@@ -1,3 +1,24 @@
 import styled from 'styled-components'
 
-export const Container = styled.div``
+export const Container = styled.div`
+display: flex;
+justify-content: center;
+align-items: center;
+height: 100vh;
+`
+
+export const Title = styled.text`
+font-size: 2rem;
+font-weight: bold;
+`
+
+export const Content = styled.div`
+display: flex;
+padding: 5rem 2rem 5rem 2rem;
+flex-direction:column;
+background-color: #f7fafe;
+width: 30%;
+border-radius: 5px;
+border: 1px solid #ddecfa;
+gap: 2rem;
+`
