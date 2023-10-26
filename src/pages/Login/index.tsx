7e82dbb9ab84a56a7a096a1cf513597d7a0a314f
unknown <noah.rissatii@gmail.com>
2023-10-25 22:14:11 -0300

    feat: criando valida

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,5 +1,5 @@
 // External Libraries
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 // Components
 
@@ -15,21 +15,47 @@ import {
 import { TitledInput } from "../../components/TitledInput";
 import { Button } from "../../components/Button";
 import { Link } from "react-router-dom";
-
-import axios from 'axios';
+import axios from "axios";
+import { IUser } from "./types";
 
 export const Login: React.FC = () => {
-  const apiUrl = 'http://localhost:3001';
+  const [formData, setFormData] = useState({
+    email: "",
+    senha: "",
+  });
+  const [users, setUsers] = useState<IUser[]>([]);
 
-  axios.get(`${apiUrl}/posts`)
-    .then(response => {
-      console.log(response.data);
-      
-    })
-    .catch(error => {
-      // Trate os erros
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
     });
+  };
 
+  const handleLogin = () => {
+    if (formData.email === '' || formData.senha === '') {
+      alert('Por favor, preencha todos os campos antes de fazer o login.');
+      return;
+    }
+
+    const user = users.find(
+      (u) => u.email === formData.email && u.senha === formData.senha
+    );
+
+    if (user) {
+      alert('Login bem-sucedido!');
+    } else {
+      alert('Email ou senha incorretos. Tente novamente.');
+    }
+  };
+ 
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/users').then((response) => {
+      setUsers(response.data);
+    });
+  }, []);
   return (
     <Container>
       <Content>
@@ -38,13 +64,26 @@ export const Login: React.FC = () => {
         </ContainerTitle>
 
         <ContainerBody>
-          <TitledInput label={"Email"} placeholder="Insira seu email..." />
-          <TitledInput label={"Senha"} placeholder="Insira sua senha" />
+          <TitledInput
+            label={'Email'}
+            placeholder='Insira seu email...'
+            name='email'
+            value={formData.email}
+            onChange={handleChange}
+          />
+          <TitledInput
+            label={'Senha'}
+            placeholder='Insira sua senha'
+            name='senha'
+            type='password'
+            value={formData.senha}
+            onChange={handleChange}
+          />
           <ContainerButtons>
-            <Link to={`register`}>
-              <Button label={"Cadastrar-se"} />
+            <Link to={'register'}>
+              <Button label={'Cadastrar-se'} />
             </Link>
-            <Button label={"Login"} />
+            <button onClick={handleLogin}>Login</button>
           </ContainerButtons>
         </ContainerBody>
       </Content>
