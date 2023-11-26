23f320e84631a44981af913334e70b9df1365217
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-26 20:35:43 -0300

    fix: changed baseUrl in final variable

--- test/src/test/java/com/test/urubupix/LoginTest.java
+++ test/src/test/java/com/test/urubupix/LoginTest.java
@@ -6,14 +6,20 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import java.time.Duration;
 import java.util.stream.Stream;
 
 public class LoginTest {
     private WebDriver driver;
-    private String baseUrl = "http://localhost:3000";
+    final String baseUrl = "http://localhost:3000";
     private LoginPage loginPage;
 
     @BeforeEach
@@ -36,6 +42,24 @@ public class LoginTest {
         assertThat(loginPage.getCurrentURL()).isEqualTo(result);
     }
 
+    @DisplayName("test the user failed login")
+    @ParameterizedTest
+    @MethodSource("provideUserFailedTest")
+    void testUserFailedLogin(String userEmail, String userPassword, String result) throws InterruptedException {
+        loginPage.writeEmailValue(userEmail);
+        Thread.sleep(1000);
+        loginPage.writePasswordValue(userPassword);
+        Thread.sleep(1000);
+        loginPage.clickLoginButton();
+        Thread.sleep(1000);
+
+        final Alert alert = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.alertIsPresent());
+        final String alertText = alert.getText();
+        alert.accept();
+        assertThat(alertText).isEqualTo(result);
+    }
+
     static Stream<Arguments> provideUserSuccessTest() {
         return Stream.of(
                 Arguments.of(
@@ -45,4 +69,15 @@ public class LoginTest {
                 )
         );
     }
+
+    static Stream<Arguments> provideUserFailedTest() {
+        return Stream.of(
+                Arguments.of(
+                        "errorerror@gmail.com",
+                        "!@#$!@",
+                        "Email ou senha incorretos. Tente novamente."
+                )
+        );
+    }
+
 }
