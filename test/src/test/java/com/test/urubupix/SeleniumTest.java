92013c848e4dcf6cc4340811f43a434fb033f149
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-26 15:58:39 -0300

    fix: changed to firefoxDriver

--- test/src/test/java/com/test/urubupix/SeleniumTest.java
+++ test/src/test/java/com/test/urubupix/SeleniumTest.java
@@ -3,14 +3,14 @@ package com.test.urubupix;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class SeleniumTest {
     @Test
     @DisplayName("Should open and close chrome browser")
     void shouldOpenAndCloseChromeBrowser() throws InterruptedException {
 // sets the driver path
-        WebDriver driver = new EdgeDriver(); // creates a driver to interact with the browser
+        WebDriver driver = new FirefoxDriver(); // creates a driver to interact with the browser
         driver.get("https://www.google.com"); // request the page
         Thread.sleep(1000); // waits for 1s.
         driver.quit(); // quits all WebDriver instances and closes the browser
