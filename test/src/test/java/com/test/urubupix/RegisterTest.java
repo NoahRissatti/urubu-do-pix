622257dad4a671e50467397d42ba8e5eac46b95b
Rafael Rocha Casaque <rrcasaque@hotmail.com>
2023-11-28 11:37:25 -0300

    feat: created userInUse test

--- test/src/test/java/com/test/urubupix/RegisterTest.java
+++ test/src/test/java/com/test/urubupix/RegisterTest.java
@@ -1,15 +1,20 @@
 package com.test.urubupix;
 ;
 import com.test.urubupix.pages.LoginPage;
+import com.test.urubupix.pages.PixType;
 import com.test.urubupix.pages.RegisterPage;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
@@ -37,10 +42,45 @@ public class RegisterTest {
         assertThat(registerPage.getCurrentURL()).isEqualTo(result);
     }
 
+    @DisplayName("test user already in use")
+    @ParameterizedTest
+    @MethodSource("provideUserInUse")
+    void testUserInUse(String userName, String userEmail, String userPassword, String result) throws InterruptedException {
+        registerPage.writeInputName(userName);
+        Thread.sleep(1000);
+        registerPage.writeInputEmail(userEmail);
+        Thread.sleep(1000);
+        registerPage.writeInputPassword(userPassword);
+        Thread.sleep(1000);
+        registerPage.writeInputPixKey(userEmail);
+        registerPage.selectTypeKey(PixType.EMAIL);
+        Thread.sleep(1000);
+        registerPage.clickRegisterButton();
+        Thread.sleep(1000);
+        Thread.sleep(1000);
+
+        final Alert alert = new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.alertIsPresent());
+        final String alertText = alert.getText();
+        alert.accept();
+        assertThat(alertText).isEqualTo(result);
+    }
+
     static Stream<Arguments> provideCancelButton() {
         return Stream.of(
                 Arguments.of(
                         "http://localhost:3000/"
+                )
+        );
+    }
+
+    static Stream<Arguments> provideUserInUse() {
+        return Stream.of(
+                Arguments.of(
+                        "teste",
+                       "teste@email.com",
+                        "1234",
+                        "O usuário já existe."
                 )
         );
     }
