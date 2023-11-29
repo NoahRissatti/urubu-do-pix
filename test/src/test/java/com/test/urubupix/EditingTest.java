aba1b095b933158c9ba62105abc6bfe4677397e4
yurIFSP <y.menon@aluno.ifsp.edu.br>
2023-11-29 00:36:18 -0300

    test: created testIfAllInputsAreCorrect test

--- test/src/test/java/com/test/urubupix/EditingTest.java
+++ test/src/test/java/com/test/urubupix/EditingTest.java
@@ -23,9 +23,9 @@ public class EditingTest {
         LoginPage loginPage = new LoginPage(driver);
         TransactionPage transactionPage = new TransactionPage(driver);
         Thread.sleep(1000);
-        loginPage.writeEmailValue("teste@email.com");
+        loginPage.writeEmailValue("teste@teste.com");
         Thread.sleep(1000);
-        loginPage.writePasswordValue("1234");
+        loginPage.writePasswordValue("123");
         Thread.sleep(1000);
         loginPage.clickLoginButton();
         Thread.sleep(1000);
@@ -36,6 +36,41 @@ public class EditingTest {
         Assertions.assertTrue(editingPage.getEmailInputValue().isEmpty());
         Assertions.assertTrue(editingPage.getPasswordInputValue().isEmpty());
         Assertions.assertTrue(editingPage.getPixInputValue().isEmpty());
+    }
+    @Test
+    public void testIfAllInputsAreCorrect() throws InterruptedException{
+        LoginPage loginPage = new LoginPage(driver);
+        TransactionPage transactionPage = new TransactionPage(driver);
+        Thread.sleep(1000);
+        loginPage.writeEmailValue("teste@teste.com");
+        Thread.sleep(1000);
+        loginPage.writePasswordValue("123");
+        Thread.sleep(1000);
+        loginPage.clickLoginButton();
+        Thread.sleep(1000);
+        transactionPage.clickUpdateButton();
+        Thread.sleep(1000);
+        EditingPage editingPage = new EditingPage(driver);
+        Thread.sleep(1000);
+        editingPage.WriteNameValue("name");
+        Thread.sleep(1000);
+        editingPage.WriteEmailValue("teste@teste.com");
+        Thread.sleep(1000);
+        editingPage.WritePasswordValue("123");
+        Thread.sleep(1000);
+        editingPage.WritePixValue("pixTest");
+        Thread.sleep(1000);
+
+        Assertions.assertEquals("name", editingPage.getNameInputValue());
+        Thread.sleep(1000);
+        Assertions.assertEquals("teste@teste.com", editingPage.getEmailInputValue());
+        Thread.sleep(1000);
+        Assertions.assertEquals("123", editingPage.getPasswordInputValue());
+        Thread.sleep(1000);
+        Assertions.assertEquals("pixTest", editingPage.getPixInputValue());
+        Thread.sleep(1000);
+
+        editingPage.clickConfirmButton();
     }
 
     @AfterEach
