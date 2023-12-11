1119400f7cdda20edea5177283f4d0de041ff65b
isaacnwt <isaacnewton02@hotmail.com>
2023-12-10 23:22:44 -0300

    feat: user model with test data mock

---
+++ test/src/test/java/models/User.java
@@ -0,0 +1,40 @@
+package models;
+
+public class User {
+    private String name;
+    private String email;
+    private String pass;
+    private String pixKey;
+    private String keyType;
+
+    public User(String name, String email, String pass, String pixKey, String keyType) {
+        this.name = name;
+        this.email = email;
+        this.pass = pass;
+        this.pixKey = pixKey;
+        this.keyType = keyType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public String getPixKey() {
+        return pixKey;
+    }
+
+    public String getKeyType() {
+        return keyType;
+    }
+    public static User getTestUser() {
+        return new User("teste", "teste@email.com", "1234", "teste@email.com", "Email");
+    }
+}
