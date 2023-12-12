f891cafe3bc9d01a6dc7b8864420e6aa43b8372d
isaacnwt <isaacnewton02@hotmail.com>
2023-12-11 23:07:32 -0300

    test: new constant added

--- test/src/test/java/config/TestConstants.java
+++ test/src/test/java/config/TestConstants.java
@@ -7,5 +7,6 @@ public class TestConstants {
     public static final String LOGIN_ERROR_MSG = "Email ou senha incorretos. Tente novamente.";
     public static final String LOGIN_EMPTY_MSG = "Por favor, preencha todos os campos antes de fazer o login.";
     public static final String REGISTER_SUCCESS_MSG = "Cadastro bem-sucedido!";
+    public static final String REGISTER_USER_ALREADY_EXISTS_MSG = "O usuário já existe.";
 
 }
